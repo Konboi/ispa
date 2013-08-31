c09767d12c15398f65d81112170db2aabae453d1
Konboi <ryosuke.yabuki@gmail.com>
2013-08-31 12:54:43 +0900

    update unicorn config

--- unicorn_config.rb
+++ unicorn_config.rb
@@ -1,2 +1,28 @@
-worker_processes 50
+# -*- coding: utf-8 -*-
+APP_ROOT     = "#{File.dirname(__FILE__)}" unless defined?(APP_ROOT)
+
+$unicorn_user  = `ls -l #{File.dirname(__FILE__)}/ | awk 'END{print $3}'`.chomp # slaveの実行ユーザ
+$unicorn_group = `ls -l #{File.dirname(__FILE__)}/ | awk 'END{print $4}'`.chomp # slaveの実行グループ
+
+
+# ---- start of config ----
+
+# タイムアウト秒数。タイムアウトしたslaveは再起動される
+$timeout = 60
+
+# String => UNIX domain socket / FixNum => TCP socket
+$listen = 5000
+
+# ---- end of config ----
+
+# Main Config for Unicorn
+worker_processes 5
 preload_app true
+timeout $timeout
+listen $listen
+
+stderr_path "#{APP_ROOT}/log/unicorn.log"
+stdout_path "#{APP_ROOT}/log/unicorn.log"
+
+
+
